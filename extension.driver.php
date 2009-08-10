5d8c8b7bb72610cac3ae6f4c80f34a84f51fbb8b
Alistair Kearney <alistair@21degrees.com.au>
2009-08-10 13:18:30 +1000

    Updated version number

--- extension.driver.php
+++ extension.driver.php
@@ -1,12 +1,11 @@
 <?php
 
-
 	Class extension_markdown extends Extension{
 	
 		public function about(){
 			return array('name' => 'Markdown and SmartyPants Text Formatter',
-						 'version' => '1.8',
-						 'release-date' => '2007-12-06',
+						 'version' => '1.9',
+						 'release-date' => '2009-08-10',
 						 'author' => array('name' => 'Alistair Kearney',
 										   'website' => 'http://www.pointybeard.com',
 										   'email' => 'alistair@pointybeard.com'),
@@ -16,4 +15,3 @@
 				
 	}
 
-?>
