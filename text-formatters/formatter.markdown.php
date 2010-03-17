7021ed924c5a0058dbb5ecec57ab62f72453c7e9
Alistair Kearney <alistair@21degrees.com.au>
2010-03-17 13:25:57 +1000

    Removed HTML purifier from the default formatter and added a new one 'Markdown (With HTML purifier)' to give the option of not using it

--- text-formatters/formatter.markdown.php
+++ text-formatters/formatter.markdown.php
@@ -23,12 +23,18 @@
 				include_once(EXTENSIONS . '/markdown/lib/php-markdown-extra-1.2.4/markdown.php');
 				self::$_parser = new Markdown_Parser();
 			}
+			
+			// Markdown transformation
 			$result = stripslashes(self::$_parser->transform($string));
 			
-			// Run the result through the HTML Purifier engine
-			include_once(EXTENSIONS . '/markdown/lib/htmlpurifier-4.0.0-standalone/HTMLPurifier.standalone.php');
-			$purifier = new HTMLPurifier;
-			return $purifier->purify($result);
+			if(!function_exists('SmartyPants')) include_once(EXTENSIONS . '/markdown/lib/php-smartypants-1.5.1e/smartypants.php');
+            
+			// Apply "Smarty Pants" formatting
+			$result = SmartyPants(
+				stripslashes(self::$_parser->transform($string))
+			);
+
+			return $result;
 		}
 		
 	}
--- text-formatters/formatter.markdown_with_purifier.php
+++ text-formatters/formatter.markdown_with_purifier.php
@@ -1,13 +1,13 @@
 <?php
 
-	Class formatterMarkdownSmartyPants extends TextFormatter{
+	Class formatterMarkdown_With_Purifier extends TextFormatter{
 
 		private static $_parser;
 
 		public function about(){
 			return array(
-				'name' => 'Markdown plus SmartyPants',
-				'version' => '1.5',
+				'name' => 'Markdown (With HTML Purifier)',
+				'version' => '1.0',
 				'release-date' => '2010-03-17',
 				'author' => array(
 					'name' => 'Alistair Kearney',
@@ -17,15 +17,19 @@
 				'description' => 'Write entries in the Markdown format. Wrapper for the PHP Markdown text-to-HTML conversion tool written by Michel Fortin.'
 			);
 		}
-	
+				
 		public function run($string){
 			if(!self::$_parser){
 				include_once(EXTENSIONS . '/markdown/lib/php-markdown-extra-1.2.4/markdown.php');
-				self::$_parser = new MarkdownExtra_Parser();
+				self::$_parser = new Markdown_Parser();
 			}
-
-            if(!function_exists('SmartyPants')) include_once(EXTENSIONS . '/markdown/lib/php-smartypants-1.5.1e/smartypants.php');
+			
+			// Markdown transformation
+			$result = stripslashes(self::$_parser->transform($string));
+			
+			if(!function_exists('SmartyPants')) include_once(EXTENSIONS . '/markdown/lib/php-smartypants-1.5.1e/smartypants.php');
             
+			// Apply "Smarty Pants" formatting
 			$result = SmartyPants(
 				stripslashes(self::$_parser->transform($string))
 			);
@@ -33,7 +37,10 @@
 			// Run the result through the HTML Purifier engine
 			include_once(EXTENSIONS . '/markdown/lib/htmlpurifier-4.0.0-standalone/HTMLPurifier.standalone.php');
 			$purifier = new HTMLPurifier;
-			return $purifier->purify($result);
+			$result = $purifier->purify($result);
+			
+			return $result;
 		}
+		
 	}
 
