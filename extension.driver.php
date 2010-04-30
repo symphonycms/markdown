8ae79e170dbe9e46125cc82a533fcd928abd3cd6
Tony Arnold <tony@tonyarnold.com>
2010-04-30 12:17:18 +1000

    Addresses issues with users upgrading to this new release after the text formatters have been renamed. (#270) http://symphony-cms.com/discuss/issues/view/270/

--- extension.driver.php
+++ extension.driver.php
@@ -14,5 +14,30 @@
 				'description' => 'Includes "Markdown" and "Markdown plus Smartypants" text formatters. Also applies the "HTML Purifier" HTML/XHTML filter on resultant output. PHP implementation of the "Markdown" text-to-HTML syntax, created by John Gruber, developed by Michel Fortin.'
 			);
 		}
+		
+		public function update($previousVersion){
+			
+			try{
+				if(version_compare($previousVersion, '1.10', '<')){
+				  
+					Symphony::Database()->query(
+						"UPDATE `tbl_fields_textarea`
+						 SET formatter='markdown' 
+						 WHERE formatter='pb_markdown'");
+					
+					Symphony::Database()->query(
+						"UPDATE `tbl_fields_textbox`
+						 SET text_formatter='markdown' 
+						 WHERE text_formatter='pb_markdown'");
+					
+				}
+			}
+			catch(Exception $e){
+				// Discard
+			}
+		
+		  return true;
+		}
+
 	}
 
