b737e99a5cd04d63681c9080ae66f7bc94e45c7b
Alistair Kearney <alistair@21degrees.com.au>
2010-04-30 12:35:53 +1000

    Small bug fixes. When there is no textbox field, the foreach look still continues with the rest of the items

--- extension.driver.php
+++ extension.driver.php
@@ -17,30 +17,36 @@
 		
 		public function update($previousVersion){
 			
-			try{
+			
 				if(version_compare($previousVersion, '1.10', '<')){
 
 					$conversion = array(
-						'pb_markdown' => 'markdown'
-						'pb_markdownextrasmartypants' => 'markdown_extra_with_smartypants'
+						'pb_markdown' => 'markdown',
+						'pb_markdownextrasmartypants' => 'markdown_extra_with_smartypants',
 						'pb_markdownextra' => 'markdown_extra'
 					);
-					
-					foreach($conversion as $old => $new){
-						Symphony::Database()->query(
-							sprintf("UPDATE `tbl_fields_textarea` SET `formatter` = '%s' WHERE `formatter` = '%s'", $new, $old)
-						);
-
-						Symphony::Database()->query(
-							sprintf("UPDATE `tbl_fields_textbox` SET `text_formatter` = '%s' WHERE `text_formatter` = '%s'", $new, $old)
-						);
+				
+					foreach($conversion as $old => $new){	
+						
+						try{		
+							Symphony::Database()->query(
+								sprintf("UPDATE `tbl_fields_textarea` SET `formatter` = '%s' WHERE `formatter` = '%s'", $new, $old)
+							);
+						}
+						catch(Exception $e){
+						}
+						
+						try{
+							Symphony::Database()->query(
+								sprintf("UPDATE `tbl_fields_textbox` SET `text_formatter` = '%s' WHERE `text_formatter` = '%s'", $new, $old)
+							);
+						}
+						catch(Exception $e){
+						}
+						
 					}
 					
 				}
-			}
-			catch(Exception $e){
-				// Discard
-			}
 		
 		  return true;
 		}
