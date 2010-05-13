9a7c19c2b3335922f6c3c95a689a629d372e0139
Alistair Kearney <alistair@21degrees.com.au>
2010-05-13 12:43:11 +1000

    Merged with local changes

--- extension.driver.php
+++ extension.driver.php
@@ -1,6 +1,6 @@
 <?php
 
-	Class extension_Markdown extends Extension{
+	Class extension_Markdown implements iExtension{
 		public function about(){
 			return (object)array(
 				'name' => 'Text Formatter: Markdown',
@@ -14,43 +14,6 @@
 				'description' => 'Includes "Markdown", "Markdown Extra", "Markdown Extra with Smartypants" and "Markdown with HTML Purifier" text formatters. PHP implementation of the "Markdown" text-to-HTML syntax, created by John Gruber, developed by Michel Fortin.'
 			);
 		}
-		
-		public function update($previousVersion){
-			
-			
-				if(version_compare($previousVersion, '1.10', '<')){
-
-					$conversion = array(
-						'pb_markdown' => 'markdown',
-						'pb_markdownextrasmartypants' => 'markdown_extra_with_smartypants',
-						'pb_markdownextra' => 'markdown_extra'
-					);
-				
-					foreach($conversion as $old => $new){	
-						
-						try{		
-							Symphony::Database()->query(
-								sprintf("UPDATE `tbl_fields_textarea` SET `formatter` = '%s' WHERE `formatter` = '%s'", $new, $old)
-							);
-						}
-						catch(Exception $e){
-						}
-						
-						try{
-							Symphony::Database()->query(
-								sprintf("UPDATE `tbl_fields_textbox` SET `text_formatter` = '%s' WHERE `text_formatter` = '%s'", $new, $old)
-							);
-						}
-						catch(Exception $e){
-						}
-						
-					}
-					
-				}
-		
-		  return true;
-		}
-
 	}
 
 	return 'extension_Markdown';
