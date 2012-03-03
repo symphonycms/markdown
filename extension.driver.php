81b76ef54f0299c522c3ee1070225e585eddaaa4
Brendan Abbott <brendan@bloodbone.ws>
2012-03-03 14:12:51 +1000

    Make update() compatible with Extension::update().

--- extension.driver.php
+++ extension.driver.php
@@ -15,41 +15,38 @@
 			);
 		}
 
-		public function update($previousVersion){
+		public function update($previousVersion = false){
+			if(version_compare($previousVersion, '1.10', '<')){
 
+				$conversion = array(
+					'pb_markdown' => 'markdown',
+					'pb_markdownextrasmartypants' => 'markdown_extra_with_smartypants',
+					'pb_markdownextra' => 'markdown_extra'
+				);
 
-				if(version_compare($previousVersion, '1.10', '<')){
+				foreach($conversion as $old => $new){
 
-					$conversion = array(
-						'pb_markdown' => 'markdown',
-						'pb_markdownextrasmartypants' => 'markdown_extra_with_smartypants',
-						'pb_markdownextra' => 'markdown_extra'
-					);
+					try{
+						Symphony::Database()->query(
+							sprintf("UPDATE `tbl_fields_textarea` SET `formatter` = '%s' WHERE `formatter` = '%s'", $new, $old)
+						);
+					}
+					catch(Exception $e){
+					}
 
-					foreach($conversion as $old => $new){
-
-						try{
-							Symphony::Database()->query(
-								sprintf("UPDATE `tbl_fields_textarea` SET `formatter` = '%s' WHERE `formatter` = '%s'", $new, $old)
-							);
-						}
-						catch(Exception $e){
-						}
-
-						try{
-							Symphony::Database()->query(
-								sprintf("UPDATE `tbl_fields_textbox` SET `text_formatter` = '%s' WHERE `text_formatter` = '%s'", $new, $old)
-							);
-						}
-						catch(Exception $e){
-						}
-
+					try{
+						Symphony::Database()->query(
+							sprintf("UPDATE `tbl_fields_textbox` SET `text_formatter` = '%s' WHERE `text_formatter` = '%s'", $new, $old)
+						);
+					}
+					catch(Exception $e){
 					}
 
 				}
 
-		  return true;
+			}
+
+			return true;
 		}
 
 	}
-
